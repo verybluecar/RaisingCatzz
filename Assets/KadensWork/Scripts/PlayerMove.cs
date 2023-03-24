c373fdbf28fd1a78f61b0c4687981edd01b4bb90
verybluecar <gamercatpoopdoodoo@gmail.com>
2023-03-24 09:36:50 -0700

    Fixed the big issue

---
+++ Assets/KadensWork/Scripts/PlayerMove.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    
+   
+
+    Vector3 veclocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+
+        // the float x and z will get the axis of your mouse
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // this will actually move the mouse 
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        //this will move the character and use the speed float to control the speed
+        controller.Move(move * speed * Time.deltaTime);
+
+        
+
+        //will move the controller down to what the gravity is from the line above 
+        controller.Move(veclocity * Time.deltaTime);
+
+       
+    }
+}
